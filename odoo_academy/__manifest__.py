3fd6c55f9f47ed393f5a555450d030f59f1c5c17
Jose Henostroza <jose.henostroza@synet.com.pe>
2022-02-17 15:55:26 +0000

    [ADD] Modulo Basico

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Odoo Academy',
+    
+    'summary' : """Academia para entrenamiento de tecnicos en software """,
+    
+    'description' : """Academia que administra los siguientes tecnicas :
+                    - Cursos
+                    - seciones
+                    - Alumnos
+     """,
+     
+    'author' : 'Odoo',
+    
+    'website' : 'http:www.synet.com.pe',
+    
+    'category' : 'Entrenamiento',
+    
+    'version' : '1.1',
+    
+    'depends' : ['base'],
+    
+    'data' : [
+        
+        
+    ],
+
+    
+    'demo' : [
+        
+        
+        
+        
+    ],
+    
+}
